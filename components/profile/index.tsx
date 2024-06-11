7cff53f529bf766b6a3aad1912391761632f4afd
Marcus <pths411027@gmail.com>
2024-06-11 22:58:41 +0800

    feat: build experience

--- components/profile/index.tsx
+++ components/profile/index.tsx
@@ -1,24 +1,42 @@
 import styles from "./profile.module.css";
-import { motion, useScroll, useTransform } from "framer-motion";
+import { jobExperience } from "../../config/text";
+import {
+  motion,
+  useScroll,
+  useTransform,
+  useMotionValue,
+  scroll,
+  useMotionValueEvent,
+  useAnimation,
+} from "framer-motion";
 export default function Profile() {
   const { scrollY } = useScroll();
-  const scale = useTransform(scrollY, [0, 200], [1, 30]);
+  const controls = useAnimation();
+  // const scale = useTransform(scrollY, [0, 200], [1, 30]);
   const marginTop = useTransform(scrollY, [0, 200], [0, 200]);
-  const marginBottom = useTransform(scrollY, [0, 200], [0, 200]);
   const opacityInOut = useTransform(
     scrollY,
     [0, 10, 190, 200], // 定义滚动位置的范围
     [1, 0, 0, 1] // 定义相应的透明度值
   );
+  const scale = useTransform(
+    scrollY,
+    [0, 200, 201], // 定义滚动位置的范围
+    [1, 30, 1] // 定义相应的透明度值
+  );
   const opacity = useTransform(scrollY, [200, 201], [1, 0]);
-  const opacity__ = useTransform(scrollY, [0, 200], [1, 0]);
   const opacity_ = useTransform(scrollY, [50, 250], [0, 1]);
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    // console.log(latest);
+    if (latest > 200)
+      controls.start({ x: 0, opacity: 1, transition: { duration: 1.5 } });
+  });
   return (
     <div className={styles.container}>
       <motion.img
         className={styles.img}
         src="123.png"
-        style={{ scale, opacity, marginTop }}
+        style={{ scale, marginTop }}
       />
 
       <motion.div style={{ opacity: opacityInOut }}>
@@ -27,6 +45,51 @@ export default function Profile() {
       <motion.div style={{ opacity: opacity_ }}>
         <h1 className={styles.name}>A Frontend Developer @ Taiwan</h1>
       </motion.div>
+      <motion.div animate={controls} style={{ opacity: 0, x: "-100%" }}>
+        <h1 className={styles.name}>Biography</h1>
+        <h4 className={styles.self_introduction}>
+          "Hi, I'm Marcus Tsai, a frontend engineer now working for Shopee. I
+          contribute to the development of web applications though React, TS and
+          Css Modules!"
+        </h4>
+      </motion.div>
+      <h1 className={styles.tech_stack}>Tech Stack</h1>
+      <div className={styles.fields}>
+        <div
+          className={styles.row_container}
+          style={{ alignItems: "center", gap: "20px" }}
+        >
+          <img className={styles.fields_Img} src="123.png" />
+          <div
+            className={styles.column_container}
+            style={{ width: "auto", padding: "10px" }}
+          >
+            <div className={styles.field} style={{ marginInline: "10px" }}>
+              Frontend Dev
+            </div>
+            <div className={styles.field_baseline} />
+          </div>
+        </div>
+      </div>
+
+      <h1 className={styles.tech_stack}>Experience</h1>
+      {jobExperience.map((job) => (
+        <div
+          className={styles.row_container}
+          style={{ alignItems: "baseline" }}
+        >
+          <div className={styles.job_duration}>{job.duration}</div>
+          <div className={styles.column_container}>
+            <div className={styles.job_title}>{job.title}</div>
+            <div className={styles.job_des}>{job.des}</div>
+            <div className={`${styles.row_container} ${styles.job_skills}`}>
+              {job.tech.map((skill) => (
+                <div className={styles.job_skill}>{skill}</div>
+              ))}
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ config/text.ts
@@ -0,0 +1,20 @@
+export const jobExperience = [
+  {
+    duration: "2023/07 - Present",
+    title: "Junior Frontend Engineer, Shopee",
+    des: "Build and maintain critical components used to construct Klaviyo’s frontend, across the whole product. Work closely with cross-functional teams, including developers, designers, and product managers, to implement and advocate for best practices in web accessibility.",
+    tech: ["React", "TypeScript", "Css Modules"],
+  },
+  {
+    duration: "2023/01 - 2023/07",
+    title: "Backend Engineer Intern, Shopee",
+    des: "Worked on the backend of the Shopee platform, focusing on the development of the search and recommendation system. Implemented a new feature that improved the search result relevance by 20%.",
+    tech: ["Python", "FastAPI", "MySQL"],
+  },
+  {
+    duration: "2022/06 - 2022/12",
+    title: "Data Analyst Intern, Nomura",
+    des: "Analyzed and visualized data to provide insights for the trading team. Developed a dashboard that improved the efficiency of data analysis by 30%.",
+    tech: ["Python", "Pandas", "PyTorch"],
+  },
+];
