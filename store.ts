21f79a869970df1ea3bda2e4a5e8aa120d7e00e1
Marcus <pths411027@gmail.com>
2024-07-04 09:35:02 +0800

    feat: build function area

--- store.ts
+++ store.ts
@@ -8,8 +8,9 @@ export interface IState {
 const useStore = create<IState>((set) => ({
   isLoadingNum: 0,
   setIsLoading: () => {
-    setTimeout(() =>
-      set((state) => ({ isLoadingNum: state.isLoadingNum + 1 }))
+    setTimeout(
+      () => set((state) => ({ isLoadingNum: state.isLoadingNum + 1 })),
+      3000
     );
   },
 }));
